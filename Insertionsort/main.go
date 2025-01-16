384495df7f3ff3bb1b582013277229fe22b91f93
Syed Azeez <syedazeez337@gmail.com>
2025-01-16 12:07:17 +0530

    insertion function

---
+++ Insertionsort/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func insertionSort(ar []int) []int {
+	// var insort []int
+
+	n := len(ar)
+
+	for i := 1; i < n; i++ {
+		current := ar[i]
+
+		j := i - 1
+		for ; j >= 0 && ar[j] > current; j-- {
+			ar[j+1] = ar[j]
+		}
+		ar[j+1] = current
+		fmt.Printf("i = %v, sort: %v\n", i, ar)
+	}
+
+	return ar
+}
+
+func main() {
+	ar := []int{61, 82, 67, 4, 98, 20, 37, 85}
+	strings.Compare()
+
+	fmt.Printf("Sorted %v\n", insertionSort(ar))
+}
