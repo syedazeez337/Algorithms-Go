fbb52d47d0b8bbf4d4583b922fe56e925dd141f6
Syed Azeez <syedazeez337@gmail.com>
2025-01-17 21:52:33 +0530

    Sorted Hashmap  function

---
+++ SortedHashmap/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Sorting by value
+
+
+
+func main() {
+	// a map
+	aMap := map[string]int{
+		"Zebra": 3,
+		"Monkey": 2,
+		"Apple": 1,
+	}
+
+	fmt.Println(aMap)
+}
